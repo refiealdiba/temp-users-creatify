80aab238aa6e4280f4f3713e29b257ac5dd94e53
pokemonnnn <93369245+pokemonnnn@users.noreply.github.com>
2025-07-11 05:08:01 +0700

    finish users

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQL_DATABASE,
 });
 
