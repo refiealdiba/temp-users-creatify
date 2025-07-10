c361da0124e2a2f0b0e4eca79d92230f71082c5c
pokemonnnn <93369245+pokemonnnn@users.noreply.github.com>
2025-07-11 05:05:49 +0700

    finish users

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,12 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "users-creatify",
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
 });
 
 export async function testDbConnection() {
